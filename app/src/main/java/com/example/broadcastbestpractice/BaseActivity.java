44b2ee6f2ba3305518c6649b4b7f7e2474fea4e3
lc8681 <lc8681@gmail.com>
2017-06-26 01:22:23 +0800

    new

--- app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
+++ app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
@@ -1,13 +1,21 @@
 package com.example.broadcastbestpractice;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by momo_lichen on 2017/6/23.
  */
 
-public class BaseActivity extends Activity {
+public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +24,47 @@ public class BaseActivity extends Activity {
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+            dialogBuilder.setTitle("提示");
+            dialogBuilder.setMessage("你的账号在其他地方登陆，请重新登陆。");
+            dialogBuilder.setCancelable(false);
+            dialogBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll(); //销毁所有Activity
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent); //重新启动LogActivity
+                }
+            });
+            dialogBuilder.show();
+        }
     }
 }
